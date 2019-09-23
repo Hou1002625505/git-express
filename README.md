eee2c692905463343d837bee4a1cf38dc856d7d1
your Name <you@example.com>
2019-09-23 15:56:53 +0800

    注册

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 git-express
+
+package-lock.json
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("../config/db");
+
+const schema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    avatar: {
+        type: String,
+        default:"http://localhost:3000/hello.jpg"
+    }
+})
+
+const model = mongoose.model("user", schema);
+
+module.exports = model;
